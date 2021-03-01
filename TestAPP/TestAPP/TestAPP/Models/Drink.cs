7f1c675386f2d3cdc8b5e0b8b0e327bfeee4cdf1
SantiagoESC <48869961+SantiagoESC@users.noreply.github.com>
2021-03-01 00:59:37 -0300

    Update
    Add class for consume a API with Drinks details

---
+++ TestAPP/TestAPP/TestAPP/Models/Drink.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAPP.Models
+{
+	public class Drink
+	{
+		#region Properties
+
+		[JsonProperty(PropertyName = "idDrink")]
+		public int IdDrink { get; set; }
+
+		[JsonProperty(PropertyName = "strDrink")]
+		public string NameDrink { get; set; }
+
+		[JsonProperty(PropertyName = "strDrinkThumb")]
+		public string ImageDrink { get; set;}
+
+		#endregion
+	}
+}
---
+++ TestAPP/TestAPP/TestAPP/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAPP.Models
+{
+	public class Response
+	{
+		#region Properties
+
+		public bool IsSuccess { get; set; }
+
+		public string Message { get; set; }
+
+		public object Result { get; set; }
+		#endregion
+	}
+}
